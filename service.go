2cb5fd84ff48fe8bdfad35ba613ece7aaf28b615
Mehran Prs <mehr.prs@gmail.com>
2020-04-20 23:41:24 +0430

    Implement base gRPC service

---
+++ service.go
@@ -0,0 +1,15 @@
+package hrpc
+
+import (
+	"context"
+	"github.com/Kamva/hexa"
+)
+
+// Service is just a base struct to use in hexa services(it's optional and you can drop it).
+type Service struct {
+}
+
+// Ctx method extract the hexa context from the context of a gRPC service
+func (r Service) Ctx(c context.Context) hexa.Context {
+	return c.Value(ContextKeyHexaCtx).(hexa.Context)
+}
